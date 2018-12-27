bfa4f5869f7ba529f3d7c5dd5388009996982711
AG <explorer_dev@163.com>
2018-12-27 12:33:20 +0800

    first commit

---
+++ TXJNetwork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TXJNetwork
+//
+//  Created by iOS_BFDL on 2018/12/27.
+//  Copyright © 2018 AG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
